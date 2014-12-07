3bd6a6338248280d67c89173c67fa3615105de0f
Sergey V <sftp.mtuci@gmail.com>
2014-12-07 18:25:22 +0300

    initial

---
+++ install/doinst.sh
@@ -0,0 +1,14 @@
+config() {
+    NEW="$1"
+    OLD="$(dirname $NEW)/$(basename $NEW .new)"
+    # If there's no config file by that name, mv it over:
+    if [ ! -r $OLD ]; then
+        mv $NEW $OLD
+    elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+        rm $NEW
+    fi
+    # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+mv etc/skel/bashrc.new etc/skel/.bashrc.new
+config etc/skel/.bashrc.new
